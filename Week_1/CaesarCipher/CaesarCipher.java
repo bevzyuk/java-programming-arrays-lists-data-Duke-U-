1badbcabf71102e512a2150c2cefc757689019ec
bevzyuk <oleksandr.bevzyuk@gmail.com>
2016-01-13 23:12:42 +0200

    Week 1 Assignments 2 done

--- Week_1/CaesarCipher/CaesarCipher.java
+++ Week_1/CaesarCipher/CaesarCipher.java
@@ -12,25 +12,25 @@ public class CaesarCipher {
        
         //Count from 0 to < length of encrypted, (call it i)
         for(int i = 0; i < encrypted.length(); i++) {
-            //Look at the ith character of encrypted (call it currChar)
-            char currChar = encrypted.charAt(i);
-            //Find the index of currChar in the alphabet (call it idx)
             int idx;
             char newChar;
+            //Look at the ith character of encrypted (call it currChar)
+         
+            char currChar = encrypted.charAt(i);
             if(Character.isLowerCase(currChar)){
-                 idx = alphabet.indexOf(Character.toUpperCase(currChar)); 
-                                
+               idx = alphabet.indexOf(Character.toUpperCase(currChar)); 
             }else{
-                 idx = alphabet.indexOf(currChar);}
+            //Find the index of currChar in the alphabet (call it idx)
+             idx = alphabet.indexOf(currChar);}
             //If currChar is in the alphabet
             if(idx != -1){
-                //Get the idxth character of shiftedAlphabet (newChar)
-              if(Character.isLowerCase(currChar)){
+                if(Character.isLowerCase(currChar)){
                newChar = Character.toLowerCase(shiftedAlphabet.charAt(idx));
-                                
             }else{
-               newChar = shiftedAlphabet.charAt(idx);}  
-               
+            //Find the index of currChar in the alphabet (call it idx)
+            newChar = shiftedAlphabet.charAt(idx); }
+                //Get the idxth character of shiftedAlphabet (newChar)
+                
                 //Replace the ith character of encrypted with newChar
                 encrypted.setCharAt(i, newChar);
             }
@@ -39,6 +39,57 @@ public class CaesarCipher {
         //Your answer is the String inside of encrypted
         return encrypted.toString();
     }
+    
+    
+    public String encryptTwoKeys(String input, int key1, int key2) {
+       StringBuilder encrypted = new StringBuilder(input);
+        //Write down the alphabet
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //Compute the shifted alphabet
+        String shiftedAlphabetKey1 = alphabet.substring(key1)+
+        alphabet.substring(0,key1);
+
+        String shiftedAlphabetKey2 = alphabet.substring(key2)+
+        alphabet.substring(0,key2);
+        //Count from 0 to < length of encrypted, (call it i)
+        for(int i = 0; i < encrypted.length(); i++) {
+            int idx;
+            char newChar;
+            //Look at the ith character of encrypted (call it currChar)
+         
+            char currChar = encrypted.charAt(i);
+            if(Character.isLowerCase(currChar)){
+               idx = alphabet.indexOf(Character.toUpperCase(currChar)); 
+            }else{
+            //Find the index of currChar in the alphabet (call it idx)
+             idx = alphabet.indexOf(currChar);}
+            //If currChar is in the alphabet
+            if(idx != -1){
+                if(Character.isLowerCase(currChar)){
+                    if(i%2==0){newChar = Character.toLowerCase(shiftedAlphabetKey1.charAt(idx));
+                    }else{
+                        newChar = Character.toLowerCase(shiftedAlphabetKey2.charAt(idx));
+                    }
+               
+            }else{
+                
+                if(i%2==0){newChar = shiftedAlphabetKey1.charAt(idx);
+                    }else{
+                       newChar = shiftedAlphabetKey1.charAt(idx);
+                    }
+            //Find the index of currChar in the alphabet (call it idx)
+             }
+                //Get the idxth character of shiftedAlphabet (newChar)
+                
+                //Replace the ith character of encrypted with newChar
+                encrypted.setCharAt(i, newChar);
+            }
+            //Otherwise: do nothing
+        }
+        //Your answer is the String inside of encrypted
+        return encrypted.toString();
+    }
+
     public void testCaesar() {
         int key = 17;
         FileResource fr = new FileResource();
@@ -48,10 +99,13 @@ public class CaesarCipher {
         String decrypted = encrypt(encrypted, 26-key);
         System.out.println(decrypted);
     }
-    
-    
+        
     public void testCaesarLowUpper() {
         System.out.println(encrypt("First Legion", 17));
+    }
+    
+     public void testCaesarTwoKey() {
+        System.out.println(encryptTwoKeys("First Legion",23, 17));
     }
 
 }
